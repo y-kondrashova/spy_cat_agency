82ecc7f67f7e73d18d7f7bc6526a098ff6bf83d0
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-11-25 13:29:42 +0200

    Add models to spy_cat app
    Models:
     - SpyCat
     - Mission
     - Target

--- spy_cat/models.py
+++ spy_cat/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class SpyCat(models.Model):
+    name = models.CharField(max_length=200)
+    year_of_experience = models.PositiveIntegerField()
+    breed = models.CharField(max_length=100)
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class Mission(models.Model):
+    cat = models.OneToOneField(
+        SpyCat,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    complete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Mission {self.id}"
+
+
+class Target(models.Model):
+    mission = models.ForeignKey(
+        Mission,
+        related_name="targets",
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=200)
+    country = models.CharField(max_length=100)
+    notes = models.TextField(blank=True)
+    complete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
